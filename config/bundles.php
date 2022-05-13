d610807a67cf23a9149b48f2375c2671352db2fd
Olha Raulet <ole4ka.safonova@gmail.com>
2022-05-13 10:44:19 +0200

    Install debug package

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
