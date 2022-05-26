f555385520d41771d21a4ae67e577be063f186d0
Olha Raulet <ole4ka.safonova@gmail.com>
2022-05-26 20:54:21 +0200

    Add roles to edit user page

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,15 @@ class UserType extends AbstractType
         $builder
             ->add('email')
             ->add('username')
-        ;
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN'
+                ],
+                'expanded' => true,
+                'multiple' => true,
+            ])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
