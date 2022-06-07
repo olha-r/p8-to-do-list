a645b021088da8c9c804a8d202c8e2c73258f344
Olha Raulet <ole4ka.safonova@gmail.com>
2022-06-07 17:21:45 +0200

    Edit Unit test of SecurityController

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -3,7 +3,6 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpFoundation\Response;
 
 class SecurityControllerTest extends WebTestCase
 {
@@ -14,51 +13,53 @@ class SecurityControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorExists('form');
-//        $this->assertSelectorTextContains('h1', 'Se connecter');
+        $this->assertSelectorTextContains('h1', 'Se connecter');
     }
-
-    public function testLoginWithBadCredentials(): void
-    {
-        $client = static::createClient();
-        $crawler =$client->request('GET', '/login');
-        $form = $crawler->selectButton('Se connecter')->form([
-            '_username' =>'john@doe.fr',
-            '_password' => 'fakepassword'
-        ]);
-        $client->submit($form);
-        $client->followRedirect();
-        $this->assertSelectorTextContains( 'div',"Invalid credentials.");
-    }
-
-
-
-
 
     public function testLoginSuccessful()
     {
         $client = static::createClient();
-        $this->loadFixtures([UserFixtures::class]);
-        $userRepository = static::$container->get(UserRepository::class);
-
-        $crawler = $client->request('GET', '/login');
+        $crawler =$client->request('GET', '/login');
         $this->assertResponseIsSuccessful();
 
         $form = $crawler->selectButton('Se connecter')->form([
-            '_username' => 'user',
+            '_username' =>'admin@gmail.com',
             '_password' => 'password'
         ]);
-
         $client->submit($form);
-        $this->assertResponseRedirects("", Response::HTTP_FOUND);
-        $crawler = $client->followRedirect();
-
-        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $this->assertResponseRedirects();
+        $client->followRedirect();
         $this->assertSelectorTextContains('h1', "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !");
-        $this->assertSelectorTextContains('strong', "user");
-        $this->assertSelectorTextContains('a.btn-danger', "Se déconnecter");
 
     }
 
+        public function testLoginWithBadCredentials(): void
+    {
+        $client = static::createClient();
+        $crawler =$client->request('GET', '/login');
+        $this->assertResponseIsSuccessful();
 
+        $form = $crawler->selectButton('Se connecter')->form([
+            '_username' =>'user@gmail.com',
+            '_password' => 'fakepassword'
+        ]);
+        $client->submit($form);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorTextContains('.alert.alert-danger', "User not found!");
+    }
+
+    public function testLogout(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/logout');
+        // Check that user is not authenticated
+        $this->assertFalse(unserialize($client->getContainer()->get('session')->get('_security_main')));
+        // Check if user is redirected to login page
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorTextContains('h1', "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !");
+        $this->assertSelectorTextContains('.btn.btn-success', "Se connecter");
+    }
 
 }
--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -2,15 +2,31 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
+    public function testTask()
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        // retrieve the test user
+        $testUser = $userRepository->findOneByEmail('admin@gmail.com');
+
+        // simulate $testUser being logged in
+        $client->loginUser($testUser);
+
+        // test e.g. the profile page
+        $client->request('GET', '/profile');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello John!');
+    }
+
     public function testTasksListPage(): void
     {
         $client = static::createClient();
         $client->request('GET', '/tasks/');
-
         $this->assertResponseIsSuccessful();
     }
 }
